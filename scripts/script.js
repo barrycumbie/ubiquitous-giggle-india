1364cdca73aea22feed0b4d71032ba1f5c5f2a5d
barrycumbie@gmail <barrycumbie@gmail.com>
2023-04-18 12:14:47 -0500

    class code stuff 1 min til the end

---
+++ scripts/script.js
@@ -0,0 +1,31 @@
+$(function () {
+  console.log("ready!");
+
+  // click a button event to load data
+
+  $("#loadProfile").on("click", function () {
+    console.log("button clicked: "); //TODO: find out who clicked me, w/this?
+
+    $.ajax({
+      url: "../assets/data.json",
+      dataType: "json",
+      success: function (data) {
+        console.log(data.userName);
+        $("#userName").val(data.userName);
+        console.log(data.optIn);
+
+        $("#optIn").prop("checked", data.optIn);
+      },
+    });
+
+    //TODO: can we grab data form anohter website.
+  });
+
+  $("#displayProfile").on("click", function () {
+    console.log("dipsplay button clicked: "); //TODO: find out who clicked me, w/
+
+    $("#dataContainer").append(
+      $("#userName").val() + " put in ... data from my input boxes"
+    );
+  });
+});
